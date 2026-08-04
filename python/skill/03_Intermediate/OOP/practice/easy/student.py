3d7a2076184d97ace7ee989d2f7531d36d984ef2
Fahim Abrar <artipeai@gmail.com>
2026-08-04 20:40:23 +0600

    practicing oop

---
+++ python/skill/03_Intermediate/OOP/practice/easy/student.py
@@ -0,0 +1,23 @@
+class Student:
+    def __init__(self, name:str, roll:int, marks:float):
+        self.name = name #student's name
+        self.roll = roll #student's roll
+        self.marks = marks #student's marks
+
+    #check pass
+    def is_pass(self) -> bool:
+        return self.marks >= 33
+
+    #display student's info
+    def display_info(self):
+        status = "Pass" if self.is_pass() else "Fail"
+
+        return (
+            f"Name: {self.name} \n"
+            f"Roll: {self.roll} \n"
+            f"Marks: {self.marks} \n"
+            f"Status: {status} \n"
+        )
+
+student_1 = Student(name="Fahim", roll=12, marks=32)
+print(student_1.display_info())
